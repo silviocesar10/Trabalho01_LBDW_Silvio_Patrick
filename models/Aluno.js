64ddb5d1fe0423d8a3bb9968c05ea2ac051c9948
Silvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2022-10-19 18:45:44 -0300

    Adicionando o arquivo modelo Aluno.js na pasta model como o novo modelo

---
+++ models/Aluno.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+const AlunoSchema = new Schema(
+    {
+        nome: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        dataNascimento: {
+            type: Date,
+            required: true,
+        },
+        sexo: {
+            type: String,
+            required: true,
+            trim: true,
+            enum: ["M", "F"],
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true,
+            index: true,
+        },
+        senha: {
+            type: String,
+            required: true,
+        },
+        ativo: {
+            type: Boolean,
+            default: false,
+        },
+        fichas: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Ficha",
+            },
+        ],
+    },
+    { timestamps: true }
+);
+export default mongoose.model("Aluno", AlunoSchema);
