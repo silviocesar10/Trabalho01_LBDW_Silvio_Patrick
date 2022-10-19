93a6bf582df0114db5b435b6b44b50f31d31ccc1
Silvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2022-10-19 18:20:49 -0300

    Adicionando a pasta de routes que contera os arquivos de rotas do projeto

---
+++ routes/routesAlunos.js
@@ -0,0 +1,38 @@
+import express from "express";
+import Aluno from "../models/AlunoModel.js";
+import { createAluno, updateAluno, deleteAluno, getAluno, getAlunos } from "../controllers/alunoController.js";
+
+const router = express.Router();
+router.post("/", async (req, res) => {
+    const aluno = new Aluno(req.body);
+    try {
+        const novoAluno = await aluno.save();
+        res.status(201).json(novoAluno);
+    } catch (error) {
+        throw error;
+    }
+});
+
+//router.post("/", (req, res) => {
+//res.send("Rota de cadastro de aluno.");
+//});
+
+router.put("/:id", (req, res) => {
+    res.send("Rota de atualização de aluno.");
+});
+router.delete("/:id", (req, res) => {
+    res.send("Rota de remoção de aluno.");
+});
+router.get("/:id", (req, res) => {
+    res.send("Rota de busca de aluno.");
+});
+router.get("/", (req, res) => {
+    res.send("Rota de listagem de alunos.");
+});
+
+router.post("/", createAluno);
+router.put("/:id", updateAluno);
+router.delete("/:id", deleteAluno);
+router.get("/:id", getAluno);
+router.get("/", getAlunos);
+export default router;
