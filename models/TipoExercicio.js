f9eae21e5c9fb3b6f6656e34b66cccbde98047a8
Silvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2022-10-19 18:47:59 -0300

    Adicionando o arquivo modelo TipoExercicio.js na pasta models

---
+++ models/TipoExercicio.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import GrupoMuscular from "./GrupoMuscular.js";
+const { Schema } = mongoose;
+const TipoExercicioSchema = new Schema({
+    nome: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    pesoMinimo: {
+        type: Number,
+        min: 0,
+        max: 100,
+        required: true,
+    },
+    pesoMaximo: {
+        type: Number,
+        min: 0,
+        max: 200,
+        required: true,
+    },
+    degrauPeso: {
+        type: Number,
+        min: 0,
+        max: 25,
+        required: true,
+    },
+    grupoMuscular: GrupoMuscular.schema,
+});
+export default mongoose.model("Exercicio", TipoExercicioSchema);
