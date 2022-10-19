e5dc136675994666d5b0242c2f6433a6bf57c463
Silvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2022-10-19 18:17:34 -0300

    Adicionando a pasta controllers que contera os controllers usados no projeto

---
+++ controllers/alunoController.js
@@ -0,0 +1,46 @@
+import Aluno from "../models/AlunoModel.js";
+
+export const createAluno = async (req, res, next) => {
+    const aluno = new Aluno(req.body);
+    try {
+        const createdAluno = await aluno.save();
+        res.status(201).json(createdAluno);
+    } catch (error) {
+        next(error);
+    }
+};
+export const updateAluno = async (req, res, next) => {
+    try {
+        const updatedAluno = await Aluno.findByIdAndUpdate(req.params.id, {
+            $set:
+                req.body
+        }, { new: true });
+        res.status(200).json(updatedAluno);
+    } catch (error) {
+        next(error);
+    }
+};
+export const deleteAluno = async (req, res, next) => {
+    try {
+        await Aluno.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: "Aluno excluído com sucesso." });
+    } catch (error) {
+        next(error);
+    }
+};
+export const getAluno = async (req, res, next) => {
+    try {
+        const aluno = await Aluno.findById(req.params.id);
+        res.status(200).json(aluno);
+    } catch (error) {
+        next(error);
+    }
+};
+export const getAlunos = async (req, res, next) => {
+    try {
+        const alunos = await Aluno.find();
+        res.status(200).json(alunos);
+    } catch (error) {
+        next(error);
+    }
+};
