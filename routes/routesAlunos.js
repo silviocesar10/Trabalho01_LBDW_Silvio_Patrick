ca3e667e32cfab9134d5f8ed61fb299ec421e619
Silvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2022-10-20 17:04:15 -0300

    Atualizando corretamente o arquivo routesAlunos.js de acordo com o retoeiro atualizado

--- routes/routesAlunos.js
+++ routes/routesAlunos.js
@@ -1,34 +1,36 @@
 import express from "express";
-import Aluno from "../models/Aluno.js";
 import { createAluno, updateAluno, deleteAluno, getAluno, getAlunos } from "../controllers/alunoController.js";
 
 const router = express.Router();
-router.post("/", async (req, res) => {
-    const aluno = new Aluno(req.body);
-    try {
-        const novoAluno = await aluno.save();
-        res.status(201).json(novoAluno);
-    } catch (error) {
-        throw error;
-    }
-});
+//router.post("/", async (req, res) => {
+//const aluno = new Aluno(req.body);
+//try {
+//const novoAluno = await aluno.save();
+//res.status(201).json(novoAluno);
+//} catch (error) {
+//  throw error;
+//}
+//});
 
 //router.post("/", (req, res) => {
 //res.send("Rota de cadastro de aluno.");
 //});
 
-router.put("/:id", (req, res) => {
-    res.send("Rota de atualização de aluno.");
-});
-router.delete("/:id", (req, res) => {
-    res.send("Rota de remoção de aluno.");
-});
-router.get("/:id", (req, res) => {
-    res.send("Rota de busca de aluno.");
-});
-router.get("/", (req, res) => {
-    res.send("Rota de listagem de alunos.");
-});
+//router.put("/:id", (req, res) => {
+//const aluno = new Aluno(req.body);
+
+//res.send("Rota de atualização de aluno.");
+//});
+
+//router.delete("/:id", (req, res) => {
+//res.send("Rota de remoção de aluno.");
+//});
+//router.get("/:id", (req, res) => {
+//res.send("Rota de busca de aluno.");
+//});
+//router.get("/", (req, res) => {
+//res.send("Rota de listagem de alunos.");
+//});
 
 router.post("/", createAluno);
 router.put("/:id", updateAluno);
