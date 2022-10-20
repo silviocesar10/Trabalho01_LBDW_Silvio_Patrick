e462ef1ef6467268315bb643ffde592e5ea26066
Silvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2022-10-20 17:12:54 -0300

    Adicionando o arquivo exercicioController.js na pasta controller como controlador das fichas

---
+++ controllers/exercicioController.js
@@ -0,0 +1,46 @@
+import Exercicio from "../models/Exercicio.js"
+
+export const createExercicio = async (req, res, next) => {
+    const aluno = new Exercicio(req.body);
+    try {
+        const createdExercicio = await aluno.save();
+        res.status(201).json(createdExercicio);
+    } catch (error) {
+        next(error);
+    }
+};
+export const updateExercicio = async (req, res, next) => {
+    try {
+        const updatedExercicio = await Exercicio.findByIdAndUpdate(req.params.id, {
+            $set:
+                req.body
+        }, { new: true });
+        res.status(200).json(updatedExercicio);
+    } catch (error) {
+        next(error);
+    }
+};
+export const deleteExercicio = async (req, res, next) => {
+    try {
+        await Exercicio.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: "Exercicio excluído com sucesso." });
+    } catch (error) {
+        next(error);
+    }
+};
+export const getExercicio = async (req, res, next) => {
+    try {
+        const aluno = await Exercicio.findById(req.params.id);
+        res.status(200).json(aluno);
+    } catch (error) {
+        next(error);
+    }
+};
+export const getExercicios = async (req, res, next) => {
+    try {
+        const alunos = await Exercicio.find();
+        res.status(200).json(alunos);
+    } catch (error) {
+        next(error);
+    }
+};
