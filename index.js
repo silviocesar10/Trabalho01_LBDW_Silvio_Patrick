c23f6892faf773d9f1941e475bf7cec917439722
Silvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2022-10-20 17:52:52 -0300

    Tentando adicionar as novas rotas ao pipeline da aplicacao

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDatabase from "./config/db.js";
 import routeAlunos from "./routes/alunosRoutes.js";
+import routeTipoExercicio from "./routes/tipoExercicioRoutes.js";
+import routeExercicio from "./routes/exercicioRoutes.js";
+import routeFicha from "./routes/fichaRoutes.js";
+import routeGrupoMuscular from "./routes/grupoMuscularRoutes.js";
+import routeInstrutor from "./routes/instrutorRoutes.js";
 import { errorHandling } from "./utils/error.js";
 
 const app = express();
@@ -10,6 +15,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(errorHandling);
 app.use("/api/alunos", routeAlunos);
+app.use("/api/tipoexercicio", routeTipoExercicio);
+app.use("/api/exercicio", routeExercicio);
+app.use("/api/ficha", routeFicha);
+app.use("/api/grupomuscular", routeGrupoMuscular);
+app.use("/api/instrutor", routeInstrutor);
+
 
 app.listen(8080, () => {
     connectDatabase();
