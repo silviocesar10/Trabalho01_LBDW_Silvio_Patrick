a8b3d1415089531a1408c772be853a0021a0e50a
Silvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2022-10-26 09:11:42 -0300

    Alterando o db.js para atender alterações feitas no .env

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connectDatabase = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL2);
+        await mongoose.connect(process.env.MONGODB_URL_LOCAL);
     } catch (error) {
         throw error;
     }
